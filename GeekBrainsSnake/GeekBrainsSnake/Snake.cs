fda3ef0d05367e52fb39f382db8f20dad58352d8
kasoo <niktrubin397sp@mail.ru>
2021-01-23 15:58:56 +0300

    fix move

--- GeekBrainsSnake/GeekBrainsSnake/Snake.cs
+++ GeekBrainsSnake/GeekBrainsSnake/Snake.cs
@@ -41,13 +41,13 @@ namespace GeekBrainsSnake
 
         public void HandleKey(ConsoleKey key)
         {
-            if (key == ConsoleKey.LeftArrow)
+            if (key == ConsoleKey.LeftArrow && direction != Direction.RIGHT)
                 direction = Direction.LEFT;
-            else if (key == ConsoleKey.RightArrow)
+            else if (key == ConsoleKey.RightArrow && direction != Direction.LEFT)
                 direction = Direction.RIGHT;
-            else if (key == ConsoleKey.UpArrow)
+            else if (key == ConsoleKey.UpArrow && direction != Direction.DOWN)
                 direction = Direction.UP;
-            else if (key == ConsoleKey.DownArrow)
+            else if (key == ConsoleKey.DownArrow && direction != Direction.UP)
                 direction = Direction.DOWN;
         }
     }
